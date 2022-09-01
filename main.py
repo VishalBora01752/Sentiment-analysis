397b1b5dca63143320195ca25effa366f0d954d2
Vishal Bora <81466944+Alias01752@users.noreply.github.com>
2022-09-01 21:05:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ from textblob import TextBlob
 
 logging.basicConfig(level=logging.DEBUG)
 
-# access token 835499518225444864-QQZBLOfwSzkC85NEWEbFNAj4vywjlQe
-# access token secret 3eRJZj9cHfHiz0rIhZsvLdC7hU9GCJcSoHwgPaUR2Hcuq
-
-# consumer key 5ILWz2QmpSPppTSOwdMvj2q5a
-# consumer secret: rKjQ8KB5QYdkq6a3Ab29n6ydc9N6YjezpdDzcALTOXaxPV1nDN
-
 access_token = "835499518225444864-QQZBLOfwSzkC85NEWEbFNAj4vywjlQe"
 access_token_secret = "3eRJZj9cHfHiz0rIhZsvLdC7hU9GCJcSoHwgPaUR2Hcuq"
 consumer_key = "5ILWz2QmpSPppTSOwdMvj2q5a"
@@ -26,13 +20,6 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Generating list of all public tweets from timeline
 public_tweets = api.home_timeline()
-
-# creating a tweet dictionary
-# tweet = public_tweets[0]
-# picking text from the tweet dictonary
-# print (tweet['text'])
-
-# tweets['text'] = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", tweets['text']).split()
 
 # Creating dataframe for holding data
 tweets_df = pd.DataFrame()
@@ -68,52 +55,6 @@ for text in tweets_df['text']:
 print("Total Positive = ", pos)
 print("Total Negative = ", neg)
 print("Total Neutral = ", neu)
-
-#def sentiment_analyser():
-#    analysis = TextBlob('.join(temp_df[].values.tolist())')
-
-    # for tweets in public_tweets:
-    #    analysis = TextBlob(-----)
-
-#    if analysis.sentiment.polarity > 0:
-#        return 'positive'
-
-#    elif analysis.sentiment.polarity == 0:
-#        return 'neutral'
-
-#    else:
-#        return 'negative'
-
-
-
-
-# picking positive tweets from tweets
-#    ptweets = []
-#    for tweet in tweets:
-#        if tweet["sentiment"] == "positive":
-#            ptweets.append(tweet)
-#    # percentage of positive tweets
-#    print("Positive tweets percentage: {} %".format(100 * len(ptweets) / len(tweets)))
-#    # picking negative tweets from tweets
-#    ntweets = []
-#    for tweet in tweets:
-#       if tweet["sentiment"] == "negative":
-#            ntweets.append(tweet)
-#    # percentage of negative tweets
-#    print("Negative tweets percentage: {} %".format(100 * len(ntweets) / len(tweets)))
-#    # percentage of neutral tweets
-#    print("Neutral tweets percentage: {} % \
-#        ".format(100 * (len(tweets) - (len(ntweets) + len(ptweets))) / len(tweets)))
-
-# printing first 5 positive tweets
-#    print("\n\nPositive tweets:")
-#    for tweet in ptweets[:10]:
-#        print(Temp_text)
-
-# printing first 5 negative tweets
-#    print("\n\nNegative tweets:")
-#    for tweet in ntweets[:10]:
-#        print(Temp_text)
 
 print(tweets_df)
 
